87756376a58f1732adf9c09d49bf2cba170c32c1
Tal Glanzman <talglanzman@gmail.com>
2023-06-02 12:39:47 +0300

    remove unneeded code

--- mypl_lex/src/token.rs
+++ mypl_lex/src/token.rs
@@ -69,35 +69,6 @@ pub enum Keyword {
     F16,
 }
 
-impl Keyword {
-    pub fn from_code(code: impl AsRef<str>) -> Option<Keyword> {
-        match code.as_ref() {
-            "cst" => Some(Keyword::Const),
-            "var" => Some(Keyword::Var),
-            "record" => Some(Keyword::Record),
-            "union" => Some(Keyword::Union),
-            "impl" => Some(Keyword::Impl),
-            "trait" => Some(Keyword::Trait),
-            "mod" => Some(Keyword::Mod),
-            "if" => Some(Keyword::If),
-            "else" => Some(Keyword::Else),
-            "for" => Some(Keyword::For),
-            "in" => Some(Keyword::In),
-            "match" => Some(Keyword::Match),
-            "return" => Some(Keyword::Return),
-            "u32" => Some(Keyword::U32),
-            "u16" => Some(Keyword::U16),
-            "u8" => Some(Keyword::U8),
-            "i32" => Some(Keyword::I32),
-            "i16" => Some(Keyword::I16),
-            "i8" => Some(Keyword::I8),
-            "f32" => Some(Keyword::F32),
-            "f16" => Some(Keyword::F16),
-            _ => None,
-        }
-    }
-}
-
 #[derive(Clone, PartialEq, Debug)]
 pub enum TokenKind {
     // =
